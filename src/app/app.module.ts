3ec4dc63542b2f93330e7c0cee70ab5cdf231c34
Tomasz Morek <t.morek@gmail.com>
2019-03-18 21:09:22 +0100

    #8 Split Home page to different component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FbWidgetComponent } from './fb-widget/fb-widget.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FbWidgetComponent
+    FbWidgetComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
