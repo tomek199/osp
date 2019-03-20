b81702261137e24e0706143b26230edca7ad9c10
Tomasz Morek <t.morek@gmail.com>
2019-03-20 22:06:47 +0100

    #8 Add alerts for pages in progress

--- src/app/fb-widget/fb-widget.component.ts
+++ src/app/fb-widget/fb-widget.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-fb-widget',
   templateUrl: './fb-widget.component.html',
   styleUrls: ['./fb-widget.component.scss']
 })
-export class FbWidgetComponent implements OnInit {
+export class FbWidgetComponent implements OnInit, OnChanges {
+
+  @Input()
+  height: number = 500;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+  }
 }
